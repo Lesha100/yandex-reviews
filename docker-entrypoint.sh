2e52763d14acc72a44b52a2a5f76a7704a2b8e01
Lesha100 <leshaokotchik@gmail.com>
2026-09-14 22:05:47 +0300

    Run queue worker with web service

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -10,6 +10,13 @@ echo "Running database seeders..."
 
 php artisan db:seed --force
 
+echo "Starting queue worker..."
+
+php artisan queue:work \
+    --sleep=1 \
+    --tries=3 \
+    --timeout=300 &
+
 echo "Starting FrankenPHP..."
 
 exec frankenphp run --config /etc/frankenphp/Caddyfile
