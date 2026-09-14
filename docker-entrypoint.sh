2ae3dafd7954bf8d8ef8c3d08e94a69674fd7a08
Lesha100 <leshaokotchik@gmail.com>
2026-09-14 21:51:13 +0300

    Seed default user on startupp

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,10 @@ echo "Running database migrations..."
 
 php artisan migrate --force
 
+echo "Running database seeders..."
+
+php artisan db:seed --force
+
 echo "Starting FrankenPHP..."
 
 exec frankenphp run --config /etc/frankenphp/Caddyfile
